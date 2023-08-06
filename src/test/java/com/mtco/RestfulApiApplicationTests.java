7425c3c59814d24d6015c65465385603752ee0c2
Süeda Özkaya <sozkaya2635@gmail.com>
2023-08-06 08:04:36 +0300

    06.08.23_V3

---
+++ src/test/java/com/mtco/RestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
